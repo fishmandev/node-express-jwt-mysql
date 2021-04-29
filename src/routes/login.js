6aa42878519a89cbab272674a25ca8d766ea5754
Dmitriy Fishman <fishman.code@gmail.com>
2021-04-29 18:09:31 +0300

    Added password hash check instead of plain text password

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const userModel = require('./../model/user');
+const bcrypt = require('bcrypt');
 
 router.post('/', (req, res, next) => {
   const email = req.body.email || '';
   const password = req.body.password || '';
 
   userModel.getUserByField('email', email).then(user => {
-    if (!user || password !== user.password)
+    if (!user || !bcrypt.compareSync(password, user.password))
       return res.status(401).json({ erorr: 'Invalid user email or password' });
 
     let token = jwt.sign({ id: user.id }, process.env.JWT_TOKEN_SECRET, {

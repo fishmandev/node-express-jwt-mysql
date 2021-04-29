00a7d17009cc2295d83c5c06d1fcdd74d7dd9852
Dmitriy Fishman <fishman.code@gmail.com>
2021-04-29 17:20:00 +0300

    Updated index route (use verify token)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const verifyToken = require('./../middleware/verifyToken');
+const userModel = require('./../model/user');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({result: 'Hellow Wrold!'});
+router.get('/', verifyToken, (req, res, next) => {
+  userModel.getUserByField('id', req.userId).then(user => {
+    if (user)
+      res.json({result: `Hello ${user.username} to the world of security!`});
+    else
+      res.json({result: `Hello deleted user to the world of security!`});
+  })
+  
 });
 
 module.exports = router;
